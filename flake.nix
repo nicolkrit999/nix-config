174d79a4ec63abf4652c9b42b7e56e307a49ab07
Ety <etienne.orio@orio.ch>
2026-02-12 19:34:53 +0100

    Clean up.

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Example nix-darwin system flake";
+  description = "Ety nix-darwin system flake";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.05-darwin";
@@ -10,32 +10,26 @@
   outputs = inputs@{ self, nix-darwin, nixpkgs }:
   let
     configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [ pkgs.vim
-        ];
+      # List packages installed in system profile
+      environment.systemPackages = [
+        pkgs.vim
+      ];
 
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
+      # Enable experimental features for flakes
+      nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-      # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
-
-      # Set Git commit hash for darwin-version.
+      # Set Git commit hash for darwin-version
       system.configurationRevision = self.rev or self.dirtyRev or null;
 
-      # Used for backwards compatibility, please read the changelog before changing.
+      # Used for backwards compatibility
       # $ darwin-rebuild changelog
       system.stateVersion = 6;
 
-      # The platform the configuration will be used on.
+      # The platform the configuration will be used on
       nixpkgs.hostPlatform = "aarch64-darwin";
     };
   in
   {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#Etiennes-MacBook-Pro
     darwinConfigurations."Etiennes-MacBook-Pro" = nix-darwin.lib.darwinSystem {
       modules = [ configuration ];
     };
